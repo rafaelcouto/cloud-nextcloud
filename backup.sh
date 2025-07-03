88431c9edb5230df7092e9107ef3107fcf7613ad
Rafael Couto <rafael@rafaelcouto.com.br>
2025-07-03 13:36:47 -0300

    ajustado para apagar arquivos de backup antigos

--- backup.sh
+++ backup.sh
@@ -6,12 +6,17 @@ while getopts "p:" arg; do
   esac
 done
 
+TARGET_DIR="/home/rafael/nextcloud/dump"
 
 # Starting maintence mode
 docker exec -u www-data nextcloud-nextcloud-app-1 php occ maintenance:mode --on
 
 # Generating database backup
-docker exec nextcloud-nextcloud-db-1 mariadb-dump --single-transaction -h localhost -u root -p$password nextcloud > "/home/rafael/nextcloud/dump/database.sql.gz"
+timestamp=$(date +"%Y%m%d%H%M%S")
+docker exec nextcloud-nextcloud-db-1 mariadb-dump --single-transaction -h localhost -u root -p$password nextcloud > "$TARGET_DIR/$timestamp.sql.gz"
+
+# Mantém apenas os últimos 7 backups
+ls -tp "$TARGET_DIR"/*.sql.gz 2>/dev/null | grep -v '/$' | tail -n +8 | xargs -r rm --
 
 # Stoping maintence mode
 docker exec -u www-data nextcloud-nextcloud-app-1 php occ maintenance:mode --off
